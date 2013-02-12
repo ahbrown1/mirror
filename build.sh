e0c5a0be0b08a210c09ecf7c268912e6c3fe4c8d
Art Brown <ahbrown1@gmail.com>
2013-02-12 01:48:44 -0600

    import java home

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -f /etc/profile.d/java.sh ] ; then
+    . /etc/profile.d/java.sh
+fi
+
 tar xvzf  commons-daemon-*-src.tar.gz
 cd commons-daemon-*-src/src/native/unix
 ./configure
